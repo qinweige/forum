7f5c0e017eabb3c18a384c2a2a58865367d29b83
weige <wgqin@reddotpay.com>
2018-03-02 14:22:36 +0800

    Threads and thread page detail

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/threads', 'threadsController@index');
+Route::get('/threads/{thread}', 'threadsController@show');
